bc9594546e56a51a072618e0f8b81e042d6be173
Samuel Pouard <50187969+samuel73000@users.noreply.github.com>
2025-06-06 00:19:25 +0200

    portfolio-v4

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,9 @@
+'use client'; // si tu es dans le dossier `/app`
 import styles from "../styles/Home.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Typewriter from 'typewriter-effect';
+
 export default function Home() {
   return (
     <section className="section-home">
@@ -8,7 +11,30 @@ export default function Home() {
       <main>
         <div className="container-home-all">
         <div className="container-home-text">
-          <h1>Hello</h1>
+          <div className="container-home-text-content-1">
+          <p className="container-home-text-content-1-text-1">hi all.I am</p>
+          <h1 className="container-home-text-content-1-text-2">Samuel Pouard</h1>
+          <div className="container-home-text-content-1-text-3">
+          <Typewriter
+  options={{
+    strings: [
+      'const role = "Développeur Web";',
+      'const stack = ["React", "Next.js"];',
+      'let freelance = true;',
+    ],
+    autoStart: true,
+    loop: true,
+    delay: 50,
+    deleteSpeed: 30,
+  }}
+/>
+</div>
+          </div>
+          <div className="container-home-text-content-2">
+          <p className="container-home-text-content-2-text-1">// complete the game to continue</p>
+          <p className="container-home-text-content-2-text-1">// find my profile on Github</p>
+          <p className="container-home-text-content-2-text-2"><span>const</span> <span className="span-home-2">githubLink</span> = <a href="https://github.com/samuel73000" className="span-home-3"> "https://github.com/samuel73000"</a></p>
+          </div>
         </div>
         <div className="container-home-game">
          game
@@ -16,6 +42,6 @@ export default function Home() {
         </div>
       </main>
       <Footer />
-    </section>
+    </section> 
   );
 }
