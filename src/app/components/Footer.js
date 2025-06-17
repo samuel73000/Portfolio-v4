b02fb4edf2eacd52045277a5d78405fa2fa9857a
Samuel Pouard <50187969+samuel73000@users.noreply.github.com>
2025-06-17 11:32:29 +0200

    footer

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -5,13 +5,13 @@ export default function Footer() {
     <footer>
       <div className='footer-all-container'>
         <div className='footer-nav-container'>
-          <p className='footer-nav-name'>Retrouvez-moi sur :</p>
+          <p className='footer-nav-name-1'>Retrouvez-moi sur :</p>
           <p className='footer-nav-hello'>
             {" "}
            <a href="https://www.linkedin.com/in/samuel-pouard/"> <img src='/icon-linkedin.svg' alt='linkedin' /></a>
           </p>
         </div>
-        <p className='footer-nav-name'>© 2025 Samuel-Pouard</p>
+        <p className='footer-nav-name-2'>© 2025 Samuel-Pouard</p>
         <div className='footer-contact-container'>
           <div className='footer-nav-contact'>
            <a href="https://github.com/samuel73000"> <span>@samuel73000</span></a>
