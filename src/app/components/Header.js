c3678e00cdec0e18bf2e3d2db393f781807df0af
Samuel Pouard <50187969+samuel73000@users.noreply.github.com>
2025-06-17 12:15:39 +0200

    header

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,14 +1,20 @@
 "use client";
 import { usePathname } from "next/navigation";
 import "../../styles/components/Header.css";
+import { useState } from "react";
 
 export default function Header() {
   const pathname = usePathname();
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
   return (
     <header>
       <div className='header-all-container'>
+        <p className='header-nav-name'>Samuel-Pouard</p>
         <div className='header-nav-container'>
-          <p className='header-nav-name'>Samuel-Pouard</p>
           <a
             href='/'
             className={`header-nav-hello${pathname === "/" ? " active" : ""}`}>
@@ -32,13 +38,65 @@ export default function Header() {
         <div className='header-contact-container'>
           <a
             href='/contact-me'
-            className={`header-nav-projects${
+            className={`header-nav-contact${
               pathname === "/contact-me" ? " active" : ""
             }`}>
             _Me_Contacter{" "}
           </a>
         </div>
+        <button className='hamburger-icon' onClick={toggleMobileMenu}>
+          &#9776; {/* Hamburger icon */}
+        </button>
       </div>
+      {isMobileMenuOpen && (
+        <div
+          className={`mobile-menu-overlay${isMobileMenuOpen ? " open" : ""}`}>
+          <div className='mobile-menu-header'>
+            <a
+              href='/'
+              className='header-nav-name-menu'
+              onClick={toggleMobileMenu}>
+              Samuel-Pouard
+            </a>
+            <button className='close-icon' onClick={toggleMobileMenu}>
+              &times; {/* Close icon */}
+            </button>
+          </div>
+          <div className='mobile-menu-links'>
+            <p className='navigate-text'># navigate:</p>
+            <a
+              href='/'
+              className={`mobile-nav-link${pathname === "/" ? " active" : ""}`}
+              onClick={toggleMobileMenu}>
+              _hello
+            </a>
+            <a
+              href='/about-me'
+              className={`mobile-nav-link${
+                pathname === "/about-me" ? " active" : ""
+              }`}
+              onClick={toggleMobileMenu}>
+              _about-me
+            </a>
+            <a
+              href='/projects'
+              className={`mobile-nav-link${
+                pathname === "/projects" ? " active" : ""
+              }`}
+              onClick={toggleMobileMenu}>
+              _projects
+            </a>
+            <a
+              href='/contact-me'
+              className={`mobile-nav-link${
+                pathname === "/contact-me" ? " active" : ""
+              }`}
+              onClick={toggleMobileMenu}>
+              _contact-me
+            </a>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
